662991337d7c676579b1e91c5509d3b4dcf06003
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-08-31 09:52:12 +0100

    style homescreen

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonText, IonButton,IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronDownOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonIcon, IonText, IonButton]
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      chevronDownOutline
+    })
+   }
 
   ngOnInit() {
   }
