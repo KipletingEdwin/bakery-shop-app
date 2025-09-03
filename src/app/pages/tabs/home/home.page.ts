9c3d9a08ea33e89c2cb04c613dc968ec471129c4
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-09-03 16:20:50 +0100

    add searchbar

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonText, IonButton,IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonText, IonButton, IonIcon, IonRow, IonCol,IonSearchbar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { chevronDownOutline } from 'ionicons/icons';
+//import { IonSearchbar } from "../../../../../node_modules/@ionic/angular/standalone/directives/searchbar";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonIcon, IonText, IonButton]
+  imports: [IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonIcon, IonText, IonButton, IonRow, IonCol, IonSearchbar]
 })
 export class HomePage implements OnInit {
 
