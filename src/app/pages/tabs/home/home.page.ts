b68a45c513b64b1c6f3a729990d0a4661ffb418a
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-09-03 18:13:32 +0100

    add searchbar

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonText, IonButton, IonIcon, IonRow, IonCol,IonSearchbar } from '@ionic/angular/standalone';
+//import { CommonModule } from '@angular/common';
+//import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonText, IonButton, IonIcon, IonRow, IonCol,IonSearchbar,IonRouterLink } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { chevronDownOutline } from 'ionicons/icons';
-//import { IonSearchbar } from "../../../../../node_modules/@ionic/angular/standalone/directives/searchbar";
+import { cart, chevronDownOutline, notificationsOutline, options } from 'ionicons/icons';
+//import { IonRouterLink } from "../../../../../node_modules/@ionic/angular/standalone/navigation/router-link-delegate";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonIcon, IonText, IonButton, IonRow, IonCol, IonSearchbar]
+  imports: [IonContent, IonHeader, IonToolbar, IonItem, IonLabel, IonIcon, IonText, IonButton, IonRow, IonCol, IonSearchbar, IonRouterLink]
 })
 export class HomePage implements OnInit {
 
   constructor() {
     addIcons({
-      chevronDownOutline
+      chevronDownOutline, cart, notificationsOutline, options
     })
    }
 
